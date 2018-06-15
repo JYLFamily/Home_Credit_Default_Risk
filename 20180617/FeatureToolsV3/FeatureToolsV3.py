ca93de99d03115fffb652c9915412112bc13702d
JYLFamily <1579502635@qq.com>
2018-06-15 19:06:17 +0800

    learn from https://www.kaggle.com/willkoehrsen/intro-to-tuning-automated-feature-engineering

---
+++ 20180617/FeatureToolsV3/FeatureToolsV3.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+
+class FeatureToolsV3(object):
+    def __init__(self, input_path, output_path):
+        self.__input_path = input_path
+        self.__output_path = output_path
+
+    def data_prepare(self):
+        pass
