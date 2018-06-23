5219c1452ae1da021e0ab590f5800181871597ac
JYLFamily <1579502635@qq.com>
2018-06-23 17:05:05 +0800

    hyper parameter tuning

--- 20180701/LightGbmKfoldFormal/BayesianOptimizationTuningParameter.py
+++ 20180701/LightGbmKfoldFormal/BayesianOptimizationTuningParameter.py
@@ -26,38 +26,36 @@ class BayesianOptimizationTuningParameter(object):
         self.__gp_params = {"alpha": 1e-5}
 
     def data_prepare(self):
-        self.__train = pd.read_csv(os.path.join(self.__input_path, "train_select_feature_df.csv"))
-        self.__train_label = self.__train["TARGET"]
-        self.__train_feature = self.__train.drop("TARGET", axis=1)
+        self.__train = pd.read_csv(os.path.join(self.__input_path, "application_train.csv"))
+        self.__train_label = self.__train["TARGET"].values.reshape((-1, ))
+        self.__train_feature = self.__train.drop(["SK_ID_CURR", "TARGET"], axis=1)
+        self.__train_feature = self.__train_feature.select_dtypes(exclude="object").values
 
-        self.__encoder = TargetEncoder()
-        self.__categorical_columns = self.__train_feature.select_dtypes("object").columns.tolist()
-        self.__encoder.fit(self.__train_feature[self.__categorical_columns], self.__train_label)
-        self.__train_feature[self.__categorical_columns] = (
-            self.__encoder.transform(self.__train_feature[self.__categorical_columns])
-        )
+        # self.__encoder = TargetEncoder()
+        # self.__categorical_columns = self.__train_feature.select_dtypes("object").columns.tolist()
+        # self.__encoder.fit(self.__train_feature[self.__categorical_columns], self.__train_label)
+        # self.__train_feature[self.__categorical_columns] = (
+        #     self.__encoder.transform(self.__train_feature[self.__categorical_columns])
+        # )
 
     def parameter_tuning(self):
-        def __cv(n_estimators, learning_rate,
-                 max_depth, num_leaves, min_split_gain, min_child_weight,
-                 colsample_bytree, subsample,
-                 reg_alpha, reg_lambda):
+        def __cv(n_estimators, learning_rate, max_depth, num_leaves, colsample_bytree, subsample, reg_alpha, reg_lambda):
             val = cross_val_score(
                 LGBMClassifier(
+                    boosting_type="goss",
                     n_estimators=int(n_estimators),
-                    learning_rate=learning_rate,
+                    learning_rate=min(learning_rate, 0.999),
                     max_depth=int(max_depth),
                     num_leaves=int(num_leaves),
-                    min_split_gain=min_split_gain,
-                    min_child_weight=min_child_weight,
-                    colsample_bytree=colsample_bytree,
-                    subsample=subsample,
+                    colsample_bytree=min(colsample_bytree, 0.999),
+                    subsample=min(subsample, 0.999),
                     reg_alpha=reg_alpha,
-                    reg_lambda=reg_lambda
+                    reg_lambda=reg_lambda,
+                    verbose=-1
                 ),
                 self.__train_feature,
                 self.__train_label,
-                "roc_auc",
+                scoring="roc_auc",
                 cv=2
             ).mean()
 
@@ -67,11 +65,9 @@ class BayesianOptimizationTuningParameter(object):
             __cv,
             {
                 "n_estimators": (500, 10000),
-                "learning_rate": (0.01, 0.05),
+                "learning_rate": (0.01, 0.1),
                 "max_depth": (4, 10),
                 "num_leaves": (10, 100),
-                "min_split_gain": (0.1, 0.999),
-                "min_child_weight": (10, 100),
                 "colsample_bytree": (0.1, 0.999),
                 "subsample": (0.1, 0.999),
                 "reg_alpha": (0.1, 0.999),
@@ -84,9 +80,15 @@ class BayesianOptimizationTuningParameter(object):
         print(self.__gbm_bo.res["max"]["max_params"]["learning_rate"])
         print(self.__gbm_bo.res["max"]["max_params"]["max_depth"])
         print(self.__gbm_bo.res["max"]["max_params"]["num_leaves"])
-        print(self.__gbm_bo.res["max"]["max_params"]["min_split_gain"])
-        print(self.__gbm_bo.res["max"]["max_params"]["min_child_weight"])
         print(self.__gbm_bo.res["max"]["max_params"]["colsample_bytree"])
         print(self.__gbm_bo.res["max"]["max_params"]["subsample"])
         print(self.__gbm_bo.res["max"]["max_params"]["reg_alpha"])
         print(self.__gbm_bo.res["max"]["max_params"]["reg_lambda"])
+
+
+if __name__ == "__main__":
+    botp = BayesianOptimizationTuningParameter(
+        input_path="C:\\Users\\puhui\\Desktop"
+    )
+    botp.data_prepare()
+    botp.parameter_tuning()
